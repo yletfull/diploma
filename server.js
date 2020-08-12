2607292311d055ec705944e562db58f21f78c736
Andrey Korlyakov <one5640@yandex.ru>
2020-08-12 20:29:56 +0300

    fix {server.js}

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-console */
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, NODE_ENV, DB_URL } = process.env;
 const { app } = require('./app');
 
 const mongooseConnection = () => {
-  const servUrl = 'mongodb://localhost:27017/mestodb';
+  const servUrl = NODE_ENV === 'production' ? DB_URL : 'mongodb://localhost:27017/mestodb';
   mongoose.connect(servUrl, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
